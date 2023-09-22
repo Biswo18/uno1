4692fa18edfccbcf768cb6d82e67895bc29001bf
Biswo81 <rajbiswopatisamal@gmail.com>
2023-09-22 15:41:16 +0530

    hello

---
+++ js/questions.js
@@ -0,0 +1,105 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is The Capital of America?",
+    answer: "C. Washington DC",
+    options: [
+      "A. Singapore",
+      "B. New-Delhi",
+      "C. Washington DC",
+      "D. Non of the Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which Animal is known as the ship  of the Desert?",
+    answer: "D. Camel",
+    options: [
+      "A. Horse",
+      "B. Donkey",
+      "C. Cow",
+      "D. Camel"
+    ]
+  },
+    {
+    numb: 3,
+    question: " In a  certain code language COMPUTER is written as RFUVQNPC. How will MEDICINE be written in that code language?",
+    answer: "D. EOJDJEFM",
+    options: [
+      "A. MFEDJJOE",
+      "B. EOJDEJFM",
+      "C. MFEJDJOE",
+      "D. EOJDJEFM"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Name of National Reptile in India?",
+    answer: "A. King-Kobra",
+    options: [
+      "A. King-Kobra",
+      "B. Anaconda",
+      "C. Python",
+      "D. Crocodile"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which Team is the first winner in Indian Premier League(IPL)?",
+    answer: "B. Rajasthan Royals",
+    options: [
+      "A. Chennai Super King",
+      "B. Rajasthan Royals",
+      "C. Royal Chalangers Banglore",
+      "D. Delhi Capitals"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Which Team is Most time Qualifier In IPL? ",
+    answer: "C. Chennai Super King",
+    options: [
+      "A. Royal Chalangers Banglore",
+      "B. Rajasthan Royals",
+      "C. Chennai Super King",
+      "D. Delhi Capitals"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "In Kabaddi How much player will play in each team?",
+    answer: "D. 7",
+    options: [
+      "A. 8",
+      "B. 9",
+      "C. 11",
+      "D. 7"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Which Team was collect most of the IPL Trophies?",
+    answer: "A. Mumbai Indians",
+    options: [
+      "A. Mumbai Indians",
+      "B. Rajasthan Royals",
+      "C. Royal Chalangers Banglore",
+      "D. Delhi Capitals"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the capital city of Kenya?",
+    answer: "A. Nairobi",
+    options: [
+      "A. Nairobi",
+      "B. Washington DC",
+      "C. Makau",
+      "D. Singapore"
+    ]
+  },
+];
